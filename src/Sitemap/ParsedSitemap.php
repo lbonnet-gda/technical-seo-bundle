4534823616a0436eb50444b9027b291e305b9919
lbonnet-gda <191601779+lbonnet-gda@users.noreply.github.com>
2026-09-17 15:02:48 +0200

    feat: add locations property with type annotations to ParsedSitemap

--- src/Sitemap/ParsedSitemap.php
+++ src/Sitemap/ParsedSitemap.php
@@ -6,6 +6,9 @@ namespace Lbonnet\TechnicalSeoBundle\Sitemap;
 
 final class ParsedSitemap
 {
+    /**
+     * @param list<string> $locations
+     */
     public function __construct(
         public readonly bool $isIndex = false,
         public readonly array $locations = [],
